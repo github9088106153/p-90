bcef2451a7a77635a6013ee6f9649452fbef3698
github9088106153 <87702995+github9088106153@users.noreply.github.com>
2022-01-12 19:40:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addUser()
+{
+    player1_name=document.getElementById("input_1").value;
+    player2_name=document.getElementById("input_2").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="game_login.html";
+}
